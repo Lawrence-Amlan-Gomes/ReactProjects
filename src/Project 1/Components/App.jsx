33bbc0953afd9d67a0cf08d5887c2ace81fb0c53
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2023-12-09 03:42:04 +0600

    updated React Project 1

--- src/Project 1/Components/App.jsx
+++ src/Project 1/Components/App.jsx
@@ -2,11 +2,18 @@ import "../Styles/App.css";
 import Layout from "./Layout";
 import Home from "./Home";
 import Signup from "./Signup";
+import Login from "./Login";
+import Quiz from "./Quiz";
+import Result from "./Result";
 
 function App() {
   return (
       <Layout>
+        <Home/>
         <Signup/>
+        <Login/>
+        <Quiz/>
+        <Result/>
       </Layout>
   )
 }
