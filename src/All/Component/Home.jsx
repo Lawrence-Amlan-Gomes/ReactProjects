5b9c84b5a24d35ff0a04df06e6d04722e8629084
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2023-12-21 01:41:32 +0600

    second commit

--- src/All/Component/Home.jsx
+++ src/All/Component/Home.jsx
@@ -1,8 +1,8 @@
-
+import {classes} from "../Styles/home.module.css";
 export default function Home() {
     return (
       <div>
-        <div>Hello Welcome to My React JS Projects</div>
+        <div className={classes.home}>Hello Welcome to My React JS Projects</div>
       </div>
     );
   }
--- src/React_17_2/Component/Theme.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const theme = React.createContext()
-
-export default theme;
