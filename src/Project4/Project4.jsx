3778290ec64a4006bd34f03db9adccd6d0b0f001
Lawrence_Amlan_Gomes <amlangomes@gmail.com>
2023-12-28 05:37:40 +0600

    project 4 running

--- src/Project4/Project4.jsx
+++ src/Project4/Project4.jsx
@@ -6,7 +6,20 @@ export default function Project4(){
         <div className={classes.box}>
           <div className={classes.subBox}>
             <div className={classes.section1}>
-               Hi Project 4 
+               To Do List
+            </div>
+            <div className={classes.section2}>
+              <div className={classes.innerSection2}>
+                <input type="text" placeholder="Write Task" className={classes.inputBox}/>
+               <button className={classes.AddButton}>Add Task</button>
+              </div>
+            </div>
+            <div className={classes.section3}>
+              <div className={classes.addedTasks}>
+                <div className={classes.taskNum}>1</div>
+                <div className={classes.taskBody}>Go to GYM </div>
+                <button className={classes.deleteButton}>X</button>
+              </div>
             </div>
           </div>
         </div>
